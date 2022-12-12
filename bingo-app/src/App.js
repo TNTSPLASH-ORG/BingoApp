75c5d36764f6765e160142008de62fceb69a907e
Skript <brody@tntsplash.net>
2022-12-12 04:19:49 +0000

    Added a simple bingo table, setup react app.

---
+++ bingo-app/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="table_div">
+      <table>
+        <tr>
+          <th>B</th>
+          <th>I</th>
+          <th>N</th>
+          <th>G</th>
+          <th>O</th>
+        </tr>
+        <tr>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+        </tr>
+        <tr>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+        </tr>
+        <tr>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+        </tr>
+        <tr>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+        </tr>
+        <tr>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+          <td>​</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
