a475d746f9b2b665c45cda09814c1948c48f5878
Skript <brody@tntsplash.net>
2022-12-15 05:00:49 +0000

    Fix Typos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import BingoCard from './BingoCard';
-import { v4 as uuidv4 } from 'uuid';
 
 function getRandomIntsInRange(min, max, count) {
   const randomInts = [];
@@ -21,7 +20,6 @@ function getRandomIntsInRange(min, max, count) {
 
 
 function App() {
-<<<<<<< HEAD
   const [bingocard, setBingoCard] = useState([])
 
   let n_numbers = getRandomIntsInRange(31, 45, 4)
@@ -30,30 +28,22 @@ function App() {
   function handleNewBingo(event) {
     setBingoCard([
       { 
-=======
-  const [bingocard] = useState([
-      {
->>>>>>> 05c68c629fb600097204b77c539b69a7d6b9d327
         "header": "B",
         "numbers": getRandomIntsInRange(1, 15, 5)
       },
       {
-        id: uuidv4(),
         "header": "I",
         "numbers": getRandomIntsInRange(16, 30, 5)
       },
       {
-        id: uuidv4(),
         "header": "N",
         "numbers": n_numbers
       },
       {
-        id: uuidv4(),
         "header": "G",
         "numbers": getRandomIntsInRange(46, 60, 5)
       },
       {
-        id: uuidv4(),
         "header": "O",
         "numbers": getRandomIntsInRange(61, 75, 5)
       }
